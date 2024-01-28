c5e41eec18b1072b2c306b6184b4b150db0dac73
Bruno Bandeira <brunodev29ab@gmail.com>
2024-01-28 13:19:04 -0300

    .

--- src/main/java/com/rabbitmq/rabbitmq/RabbitmqApplication.java
+++ src/main/java/com/rabbitmq/rabbitmq/RabbitmqApplication.java
@@ -13,6 +13,5 @@ public class RabbitmqApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RabbitmqApplication.class, args);
 	}
-	// https://www.youtube.com/watch?v=rRupk_WUIBI&list=PLyUWi6NKA47v55hInmFDnT6lNB6puAuwF&index=54&ab_channel=CaioRodrigues-BackEndJava
-	//  01:05
+
 }
